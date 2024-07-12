1ca95e5f20a5890b34e96403b3c2fa48d9d2ac2b
cdestremau <157820618+cdestremau@users.noreply.github.com>
2024-07-12 16:49:57 +0200

    RF application on SACROIS data

--- Scripts/rf_sacrois_model_application.R
+++ Scripts/rf_sacrois_model_application.R
@@ -1,9 +1,7 @@
 library(data.table)
-
+library(stringr)
+library(dplyr)
 SACROIS=data.table()
-
-
-
 library(data.table)
 
 
@@ -13,42 +11,49 @@ setwd("C:/Users/cdestrem/Documents/Data/SACROIS")
 
 # Loop over the years 2007 to 2023
 
-for (i in 2007:2023) {  
+#data treatment from 2007 to 2022 :
+
+SACROIS=data.table()
+
+
+
+for (i in 2007:2023) {  #the year 2023 is included to take into account the marees beginning in 2022 and ending in 2023 (including not negligible amount of data about the end of 2022)
   
-  # Set working directory for the specific year
-  setwd(paste0('C:/Users/cdestrem/Documents/Data/SACROIS/', i))
+  setwd(paste0('C:/Users/cdestrem/Documents/Data/SACROIS/',i));
   
-  # Read the data file and bind to SACROIS
-  SACROIS <- rbind(SACROIS, fread(paste0("NAVIRES-MOIS-MAREES-JOUR-IFR_", i, ".txt"), dec=","), fill=TRUE)
+  SACROIS<-rbind(fread(paste0("NAVIRES-MOIS-MAREES-JOUR-IFR_",i,".txt"), dec=",", select= c("NAVS_COD","MAREE_ID", "SEQ_ID","DATE_SEQ", "MAILLAGE","DIMENSION","SECT_COD_SACROIS_NIV3","SECT_COD_SACROIS_NIV5",
+                                                                                                        "TP_NAVIRE_SACROIS","ESP_COD_FAO","STOCK_ORGP","QUANT_POIDS_VIF_SACROIS", "ESP2_COD_FAO", "METIER_COD_SACROIS"
+)), SACROIS);
   
-  # Remove vessels without id
-  SACROIS <- SACROIS[NAVS_COD != ""]
+  SACROIS=SACROIS[NAVS_COD!="",];         #removal of vessels without id
   
-  # Produce a unique ID per vessel and per year
-  SACROIS[, NAVS_COD_YEAR := paste(NAVS_COD, substr(DATE_SEQ, 7, 10), sep = "_")]
+  SACROIS[,NAVS_COD_YEAR:=paste(SACROIS[,NAVS_COD], str_sub(SACROIS[,DATE_SEQ],7,10), sep="_")];   #produce a unique ID per vessel ad per year
   
-  combined_select <- tapply(SACROIS[, SECT_COD_SACROIS_NIV3], SACROIS[, NAVS_COD_YEAR], 
-                          function(x) any(x %in% c("27.7.e", "27.7.d")) |
-                            any(SACROIS[NAVS_COD_YEAR == x, SECT_COD_SACROIS_NIV4] %in% c("27E3", "27E4", "26E4", "26E3", "25E3", "25E4")))
+  select_biscay=
+    tapply(SACROIS[,SECT_COD_SACROIS_NIV3], SACROIS[,NAVS_COD_YEAR],
+           function(x) ("27.7.e" %in% x | "27.7.d" %in% x | "27.7.h" %in% x) );
   
-  SACROIS <- SACROIS[NAVS_COD_YEAR %in% combined_select]
+  SACROIS=
+    SACROIS[NAVS_COD_YEAR %in% names(select_biscay[select_biscay]),];      #selection of vessels with activity in the bay of biscay (discarding vessels the years they have no activity in the bay of Biscay)
   
-  # Remove the extra column to be able to bind data table
-  SACROIS[, NAVS_COD_YEAR := NULL]
+  SACROIS[,NAVS_COD_YEAR:=NULL]     #removing the extra column to be able to bind data table
 }
 
-# Produce a unique ID per vessel and per year again at the end
-SACROIS[, NAVS_COD_YEAR := paste(NAVS_COD, substr(DATE_SEQ, 7, 10), sep = "_")]
 
+# Produce a unique ID per vessel and per year again at the end
+#SACROIS[, NAVS_COD_YEAR := paste(NAVS_COD, substr(DATE_SEQ, 7, 10), sep = "_")]
+#data_sacrois[, NAVS_COD_YEAR := paste(NAVS_COD, substr(DATE_SEQ, 7, 10), sep = "_")]
 # Print the resulting data table (optional)
+
 print(SACROIS)
 
+SACROIS <- SACROIS[,NAVS_COD_YEAR:=paste(SACROIS[,NAVS_COD], str_sub(SACROIS[,DATE_SEQ],7,10), sep="_")]
 
+# 'C:/Users/cdestrem/Documents/Data/SACROIS/'
+# "NAVS_COD","MAREE_ID", "SEQ_ID","DATE_SEQ", "MAILLAGE","DIMENSION","SECT_COD_SACROIS_NIV3","SECT_COD_SACROIS_NIV4","TP_NAVIRE_SACROIS","ESP_COD_FAO","STOCK_ORGP","QUANT_POIDS_VIF_SACROIS"
 
-
-
-
-
+SACROIS <- SACROIS %>% filter(SECT_COD_SACROIS_NIV3 %in% c("27.7.e", "27.7.d") | 
+                                     (SECT_COD_SACROIS_NIV3 == "27.7.h" & SECT_COD_SACROIS_NIV5 %in% c("27E3","27E4","26E4","26E3","25E3","25E4")))
 # Set working directory
 setwd("~/Data/SACROIS")
 # Load FPC data
@@ -93,9 +98,15 @@ library(randomForest)
 #data <- BYC[!duplicated(ID_CAPTURE),] 
 
 # Grabbing the necessary columns, most of these ended up not being used for analysis
+# NAVP_LONGUEUR_HT
+# DCR_FLOTTILLE_LIB
+# NAVP_PUISSANCE_AD
 
 data_sacrois <- SACROIS %>%
-  select(MAREE_ID,DCR_FLOTTILLE_LIB,ESP_COD_FAO, MAILLAGE, NAVP_LONGUEUR_HT, TP_NAVIRE_SACROIS, QUANT_POIDS_VIF_SACROIS, NAVP_PUISSANCE_AD, SEQ_ID,TP_NAVIRE_MOYENNE)
+  select(NAVS_COD_YEAR,DCR_FLOTTILLE_LIB, DATE_SEQ, MAREE_ID,ESP_COD_FAO, MAILLAGE,NAVP_LONGUEUR_HT,
+         NAVP_PUISSANCE_AD, TP_NAVIRE_SACROIS, QUANT_POIDS_VIF_SACROIS, SEQ_ID,SECT_COD_SACROIS_NIV3, METIER_COD_SACROIS)
+
+
 
 # Grabbing total weight per marées
 
@@ -105,10 +116,21 @@ data_sacrois <- data_sacrois %>%
   ungroup()
 
 
+#maree_sacrois <- data_sacrois %>% distinct(MAREE_ID, .keep_all = T) %>%
+#  group_by(DCR_FLOTTILLE_LIB, ESPECE_CIBLE)
+#summarize (n_maree = n_distinct(MAREE_ID))
+
+
+
 # Adjust here type of Flottille analysis will be conducted on 
 
 pelagique <- data_sacrois %>% filter (DCR_FLOTTILLE_LIB == "Chalutiers pélagiques")
 
+fond <- data_sacrois %>% filter (DCR_FLOTTILLE_LIB == "Chalutiers de fond") %>% distinct(MAREE_ID, .keep_all = T)
+
+fileyeurs <- data_sacrois %>% filter (DCR_FLOTTILLE_LIB == "Fileyeurs")%>% distinct(MAREE_ID, .keep_all = T)
+senneurs <- data_sacrois %>% filter (DCR_FLOTTILLE_LIB == "Senneurs")%>% distinct(MAREE_ID, .keep_all = T)
+casiers <- data_sacrois %>% filter (DCR_FLOTTILLE_LIB == "Casiers et pièges")%>% distinct(MAREE_ID, .keep_all = T)
 
 
 #pelagique_ptmbss <- pelagique_ptmbss %>% 
@@ -133,7 +155,12 @@ pelagique$QUANT_POIDS_VIF_SACROIS <- as.numeric(pelagique$QUANT_POIDS_VIF_SACROI
 
 # periodically check of NAs
 colSums(is.na(pelagique))
-#table(pelagique_ptmbss$Bycatch)
+
+unique_pelagique <- pelagique %>% distinct(MAREE_ID, .keep_all = T)
+colSums(is.na(unique_pelagique))
+
+# 28% of marées don't have QUANT_POIDS_VIF_SACROIS
+# 25% of marées don't have TP_NAVIRE_SACROIS 
 
 
 library(tidyr)
@@ -241,20 +268,24 @@ pelagique_sacrois <- pelagique_sacrois %>%
   left_join(seq, by = "MAREE_ID")
 
 
+
+
+
+
 maree_summary_sacrois <- pelagique_sacrois %>%
   filter(!is.na(ESP_COD_FAO)) %>%
   group_by(MAREE_ID) %>%
-  summarise(n_especes = n_distinct(ESP_COD_FAO),
-            av_mail = mean(av_mail,na.rm = T),
-            av_peche = mean(av_peche,  na.rm = T), 
-            Longueur = unique(NAVP_LONGUEUR_HT) / 100,
-            av_puissance = unique(NAVP_PUISSANCE_AD),
-            poids_tot = round(sum(as.numeric(Poids_tot_MAREE), na.rm = TRUE)),
+  summarise(n_species = n_distinct(ESP_COD_FAO),
+            mean_mesh = mean(av_mail,na.rm = T),
+            mean_fishing = mean(av_peche,  na.rm = T), 
+            boat_size = unique(NAVP_LONGUEUR_HT) / 100,
+            mean_power = unique(NAVP_PUISSANCE_AD),
+            total_catch_weight = round(sum(as.numeric(Poids_tot_MAREE), na.rm = TRUE)),
             #first_sp_fished = names(sort(tapply(POIDS_REF, ESPF_COD, sum, na.rm = TRUE), decreasing = TRUE))[1],
-            poids_total_sp_plus_peche = max(tapply(QUANT_POIDS_VIF_SACROIS, ESP_COD_FAO, sum, na.rm = TRUE), na.rm = TRUE),
+            weight_sp_fished = max(tapply(QUANT_POIDS_VIF_SACROIS, ESP_COD_FAO, sum, na.rm = TRUE), na.rm = TRUE),
             #stat_rectangle = names(sort(tapply(POIDS_REF, RECTANGLE, sum, na.rm = TRUE), decreasing = TRUE))[1],
             #month = names(sort(tapply(POIDS_REF, Month, sum, na.rm = TRUE), decreasing = TRUE))[1],
-            fam_plus_peche = names(sort(tapply(QUANT_POIDS_VIF_SACROIS, FAMILLE, sum, na.rm = TRUE), decreasing = TRUE))[1],
+            fam_fished = names(sort(tapply(QUANT_POIDS_VIF_SACROIS, FAMILLE, sum, na.rm = TRUE), decreasing = TRUE))[1],
             #total_w_fam_fished = max(tapply(POIDS_REF, FAMILY_COD_FAO, sum, na.rm = TRUE), na.rm = TRUE),
             #Months = paste(Month, collapse = ", "),
             #Etat_mer = paste(ETAT_MER, collapse = ", "),
@@ -263,11 +294,6 @@ maree_summary_sacrois <- pelagique_sacrois %>%
   ) %>%
   ungroup() %>%
   left_join(sp_p_w_sacrois, by = "MAREE_ID")
-
-
-# Put qualitative variables as.factor() for analysis 
-maree_summary_sacrois$n_especes <- as.numeric(maree_summary_sacrois$n_especes)
-#maree_summary$first_fam_fished <- as.factor(maree_summary$first_fam_fished)
 
 
 
@@ -285,10 +311,10 @@ colSums(is.na(maree_summary_sacrois))
 
 # Put whatever is not in correct format in the correct format
 
-maree_summary_sacrois$n_especes <- as.numeric(maree_summary_sacrois$n_especes)
-maree_summary_sacrois$Longueur <- as.numeric(maree_summary_sacrois$Longueur)
+maree_summary_sacrois$n_species <- as.numeric(maree_summary_sacrois$n_species)
+maree_summary_sacrois$boat_size <- as.numeric(maree_summary_sacrois$boat_size)
 #maree_summary$sp_plus_peche <- as.factor(maree_summary$sp_plus_peche)
-maree_summary_sacrois$fam_plus_peche <- as.factor(maree_summary_sacrois$fam_plus_peche)
+maree_summary_sacrois$fam_fished <- as.factor(maree_summary_sacrois$fam_fished)
 
 # One last check for NA for good measure
 
@@ -296,30 +322,42 @@ colSums(is.na(maree_summary_sacrois))
 
 #maree_summary_sacrois$Bycatch <- NA  # Or any default value
 #maree_summary_sacrois$Bycatch <- as.factor(maree_summary_sacrois$Bycatch)
-maree_summary$fam_plus_peche <- as.character(maree_summary$fam_plus_peche)
-maree_summary_sacrois$fam_plus_peche <- as.character(maree_summary_sacrois$fam_plus_peche)
+maree_summary$fam_fished <- as.character(maree_summary$fam_fished)
+maree_summary_sacrois$fam_fished <- as.character(maree_summary_sacrois$fam_fished)
 
 
 ##############################################################
 
 # RandomForest (classification)
 
-bycatch_events <- predict(rf_byc, newdata = maree_summary_sacrois)
+bycatch_events <- predict(rf_byc, newdata = maree_summary_sacrois, type = "response")
 
 maree_summary_sacrois$bycatch_prediction <- bycatch_events
-# 246 events predicted
+# 637 events predicted / 10967 events total
+
 
 
 # RandomForest (regression)
 ## Must re-run the maree_summary_sacrois chunk before running the RF
 
-bycatch_numbers <- predict(rf_byc_nb, newdata = maree_summary_sacrois)
+bycatch_numbers <- predict(rf_byc_nb, newdata = maree_summary_sacrois[,-16])
 
 maree_summary_sacrois$bycatch_nb_predictions <- bycatch_numbers
 
+round(maree_summary_sacrois$bycatch_prediction)
 
 sum(maree_summary_sacrois$bycatch_nb_predictions)
-#2018.994 dolphins
+# 4906.809 dolphins
+# 289 ind/year 
+
+
+fam <- maree_summary_sacrois %>%
+  group_by(fam_plus_peche) %>%
+  summarise(ind = sum(bycatch_nb_predictions))
+
+##
+
+
 
 ###################################################################
 
@@ -331,12 +369,39 @@ sum(maree_summary_sacrois$av_peche)
 
 # Total fishing effort (in days) of ObsMer is equal to 34.40146
 
-BPUE <- (69/34.40146) * 1173.414
+BPUE <- (69/34.40146) * 2078.393
 
-# 2353.55 dolphins captured by pelagic trawlers between 2007 and 2023 
+# 664.57 bycatch events 
+# 4168.693 dolphins captured by pelagic trawlers between 2007 and 2023 
+# which gives us about 245 ind/year 
 
-# which gives us about 147ind/year 
+# strat use package class and knn to predict clusters in sacrois 
+# knn does not handle categorical variables
 
 
 
+bycatch_clust <- predict(rf_byc_clust, newdata = maree_summary_sacrois[,-16])
 
+maree_summary_sacrois$clust_prediction <- bycatch_clust
+
+byc_sacrois_clust <- maree_summary_sacrois %>% filter (clust_prediction == "1")
+
+sum(byc_sacrois_clust$av_peche) 
+
+BPUE_strat <- (69/23.19676) 
+
+# 4194.852
+
+
+
+##########
+
+
+
+pelagique$DATE_SEQ <- dmy_hms(pelagique$DATE_SEQ)
+
+# Extract month and year into separate columns
+pelagique$Month <- month(pelagique$DATE_SEQ)
+pelagique$Year <- year(pelagique$DATE_SEQ)
+
+unique_pelagique <- pelagique %>% distinct(MAREE_ID, .keep_all = T)
